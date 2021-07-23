3c39cd48a74ffa4186623969c3e4af6439d8cb54
Nicholas Mayne <Jetski5822@gmail.com>
2021-07-23 14:15:01 +0100

    Initial Commit to replicate Serialization issues

---
+++ Controllers/TestPatchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Json.Patch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("patchme")]
+    public class TestPatchController : ControllerBase
+    {
+        [HttpPatch]
+        public async Task<IActionResult> Patch(
+            [FromBody] JsonPatch clockingPatch)
+        {
+            return Ok();
+        }
+    }
+}
